6ae375e913f253f5c09c2e792977af79a9403429
pulsarfox <pascal@plumia.net>
2024-10-01 20:40:11 +0200

    Init repository

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'social-media', loadChildren: () => import('./social-media/social-media.module').then(m => m.SocialMediaModule) },
+  { path: '**', redirectTo: 'social-media' }
+];
