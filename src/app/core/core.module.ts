b0a4e2a8aabe71c17b2c939bcb45a04879d89314
pulsarfox <pascal@plumia.net>
2024-10-02 19:45:24 +0200

    Add header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SharedModule} from "../shared/shared.module";
+import {HeaderComponent} from "./components/header/header.component";
+import {RouterModule} from "@angular/router";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+
+  ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    HeaderComponent,
+    RouterModule
   ],
   exports: [
-
+    HeaderComponent,
   ]
 })
 export class CoreModule { }
