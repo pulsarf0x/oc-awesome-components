4ee152939a64e318348a902ee6b18f1ab41dcf27
pulsarfox <pascal@plumia.net>
2024-11-28 20:16:36 +0100

    Split animations files

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -4,7 +4,20 @@ import {DatePipe, NgForOf, NgIf} from "@angular/common";
 import {SharedModule} from "../../shared.module";
 import {FormBuilder, FormControl, Validators} from "@angular/forms";
 import {TimeAgoPipe} from "../../pipes/time-ago.pipe";
-import {animate, state, style, transition, trigger} from "@angular/animations";
+import {
+  animate,
+  animateChild,
+  group,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger, useAnimation
+} from "@angular/animations";
+import {flashAnimation} from "../../animations/flash.animations";
+import {slideAndFadeAnimation} from "../../animations/slide-and-fade.animation";
 
 @Component({
   selector: 'app-comments',
@@ -19,6 +32,15 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.scss',
   animations: [
+    trigger('list', [
+      transition(':enter', [
+        query('@listItem', [
+          stagger(50, [
+            animateChild()
+          ])
+        ])
+      ])
+    ]),,
     trigger('listItem', [
       state('default', style({
         transform: 'scale(1)',
@@ -35,6 +57,37 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
       ]),
       transition('active => default', [
         animate('500ms ease-in-out')
+      ]),
+      transition(':enter', [
+        query('.comment-text, .comment-date', [
+          style({
+            opacity: 0
+          })
+        ]),
+        useAnimation(slideAndFadeAnimation, {
+          params: {
+            time: '5004ms',
+            startColor: 'rgb(201, 157, 242)'
+          }
+        }),
+        group([
+          useAnimation(flashAnimation, {
+            params: {
+              time: '250ms',
+              flashColor: 'rgb(249,179,111)'
+            }
+          }),
+          query('.comment-text', [
+            animate('250ms', style({
+              opacity: 1
+            }))
+          ]),
+          query('.comment-date', [
+            animate('500ms', style({
+              opacity: 1
+            }))
+          ]),
+        ]),
       ])
     ])
   ]
@@ -63,6 +116,13 @@ export class CommentsComponent implements OnInit{
       return
     }
 
+    const maxId = Math.max(...this.comments.map(comment => comment.id))
+    this.comments.unshift({
+      id: maxId + 1,
+      comment: this.commentCtrl.value,
+      createdDate: new Date().toISOString(),
+      userId: 1
+    })
     this.newComment.emit(this.commentCtrl.value)
     this.commentCtrl.reset()
   }
