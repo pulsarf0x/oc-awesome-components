0b92951b0fbaa01b802548a96225d66335043498
pulsarfox <pascal@plumia.net>
2025-01-23 16:44:51 +0100

    Fix comment animation

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {MatToolbar} from "@angular/material/toolbar";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    MatToolbar
+    MatToolbar,
+    RouterLink
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -66,7 +66,7 @@ import {slideAndFadeAnimation} from "../../animations/slide-and-fade.animation";
         ]),
         useAnimation(slideAndFadeAnimation, {
           params: {
-            time: '5004ms',
+            time: '500ms',
             startColor: 'rgb(201, 157, 242)'
           }
         }),
